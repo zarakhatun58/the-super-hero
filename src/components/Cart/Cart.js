6cdb2135b7722e917559c60db479f00db8405cfd
jahanara khatun <zarakhatun58@gmail.com>
2021-09-27 15:21:18 +0530

    all are added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,74 @@
 import React from 'react';
+import DoctorShow from '../Cart/DoctorShow';
 
 const Cart = (props) => {
-    const { cart } = props;
+    const { cart, removeDoctor } = props;
 
-    let total = 0;
-    for (const doctor of cart) {
-        total = total + doctor.money;
+    // let total = 0;
+    // for (const doctor of cart) {
+    //     total = total + doctor.money;
+    // }
+    let totalMoney = 0;
+    if (cart.length) {
+        totalMoney += cart.reduce(
+            (previus, current) => previus + current.money,
+            0
+        );
     }
     return (
-        <div>
-            <h2> Fund Summery:{ }</h2>
+        <div className=" ">
+            <h2> Fund Summery{ }</h2>
             <h3><i class="fas fa-user"></i>Total Members:{props.cart.length}</h3>
 
-            <h4> Total Money:{total}</h4>
+            <h4> Total Money:{totalMoney} $</h4>
+            <div className="single-section">
+                {
 
+                    cart.map((cart, index) => <DoctorShow carts={cart}
+                        index={index}
+                        key={Math.random()}
+                        removeDoctor={removeDoctor}> </DoctorShow>
+                    )
+                }
+            </div>
         </div>
     );
 };
 
 export default Cart;
+
+
+// import React from "react";
+// import DoctorShow from "./DoctorShow";
+// import "./Cart.css";
+// const Cart = (props) => {
+//     const { carts, removeDoctor } = props;
+//     let totalMoney = 0;
+//     if (carts.length) {
+//         totalMoney += carts.reduce(
+//             (previus, current) => previus + current.money,
+//             0
+//         );
+//     }
+//     return (
+//         <div className="card">
+//             <div className="card-body">
+//                 <h4 className="card-title">
+//                     Doctors:{props.carts.length}
+//                 </h4>
+//                 <h5 className="card-title">Total Cost : {totalMoney} $ </h5>
+//                 {carts.map((cart, index) => (
+//                     <DoctorShow
+//                         carts={cart}
+//                         index={index}
+//                         key={Math.random()}
+//                         removeDoctor={removeDoctor}
+//                     />
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Cart;
+
--- src/components/Cart/DoctorShow.js
+++ src/components/Cart/DoctorShow.js
@@ -1,24 +1,80 @@
 import React from 'react';
+import './DoctorShow.css'
 
 const DoctorShow = (props) => {
     const { name, img } = props.carts;
     return (
-        <div>
-            <div>
-                <img src={img} alt="..." />
-            </div>
-            <div>
-                <p> {name}</p>
-                <button
-                    onClick={() => props.removeActor(props.index)}
-                    className=""
-                > <i class="fas fa-window-close"></i>
+        <div style={{ display: "flex", }} className="">
 
-                </button>
+            <div>
+                <img src={img} alt=" " style={{ width: "50px", borderRadius: "50%", marginRight: "30px", marginLeft: "30px", marginTop: "10px" }} />
             </div>
+            <div>
+                <h4> {name}</h4>
+
+            </div>
+
+
+
+
+
         </div>
     );
 };
 
 export default DoctorShow;
+
+/*
+
+
+<ul style={{ display: "inline", listStyleType: "none" }}>
+    <li> <img src={img} alt=" " style={{ width: "50px", borderRadius: "50%" }} /></li>
+    <li> {name}</li>
+    <li><button
+        onClick={() => props.removeDoctor(props.index)}
+        className="btn-2"
+    > <i class="fas fa-window-close"></i>
+
+    </button> </li>
+
+</ul>
+*/
+
+
+/*
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const ActorShow = (props) => {
+  const { name, img } = props.carts;
+  return (
+    <div className="card mb-3">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img
+            src={img}
+            className="img-fluid cart_img rounded-start"
+            alt="..."
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body d-flex justify-content-between">
+            <p className="card-title m-0"> {name}</p>
+            <button
+              onClick={() => props.removeActor(props.index)}
+              className="btn btn-sm btn-danger"
+            >
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActorShow;
+
+*/
 
